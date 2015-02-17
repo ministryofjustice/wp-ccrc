5aedc660561c9efb05b7b37c24af6677570b254c
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-02-17 15:52:22 +0000

    Added language links in footer

--- wp-content/themes/ccrc-theme/templates/footer.php
+++ wp-content/themes/ccrc-theme/templates/footer.php
@@ -1,7 +1,9 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-    <div id="footer-links">
+	<div id="lang-links">
+   	 <?php dynamic_sidebar('sidebar-footer'); ?>
+    </div>
+   <div id="footer-links">
     <a href="#">Privacy statement</a> | <a href="#">Justice on GOV.UK</a> | <a href="#">Sitemap</a>
     </div>
   </div>
