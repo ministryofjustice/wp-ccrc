2714c4d952ad158868f7c2e97f902c88a050711e
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-02-18 16:24:06 +0000

    Added menus and some static content into footer

--- wp-content/themes/ccrc-theme/templates/footer.php
+++ wp-content/themes/ccrc-theme/templates/footer.php
@@ -1,12 +1,26 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
-	<div id="lang-links">
-   	 <?php dynamic_sidebar('sidebar-footer'); ?>
-    </div>
-   <div id="footer-links">
-    <a href="#">Privacy statement</a> | <a href="#">Justice on GOV.UK</a> | <a href="#">Sitemap</a>
-    </div>
+  <div class="row">
+	  <div class="col-sm-9" id="footer-about">
+	     	 <h4>Crimial Cases Review Commission</h4>
+         <p>The Criminal Cases Review Commission is the independent public body set up to investigate possible miscarriages of justice. The Commission assesses whether convictions or sentences should be referred to a court of appeal.</p>
+	  </div>
+	    <div class="col-sm-3" id="footer-contact">
+     	 <h4>CCRC contact details</h4>
+        <p>Alpha Tower,<br>
+        Suffolk Street Queensway,<br>
+        Birmingham, B1 1TT<br>
+        DX: 715466 Birmingham 41
+        </p>
   </div>
+  </div>
+
+	<div id="lang-links">
+   	 <?php wp_nav_menu( array('menu' => 'Footer langauge links' )); ?>
+    </div>
+    <div id="footer-links">
+    <?php wp_nav_menu( array('menu' => 'Footer links' )); ?>
+    </div>   </div>
 </footer>
 
 <?php wp_footer(); ?>
