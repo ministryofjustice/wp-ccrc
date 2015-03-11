f46097fc8cfe0821b8c6c9a5e19e9893d1377e8c
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-03-11 14:30:41 +0000

    Replaced header pngs with svgs (with png fallbacks)

--- wp-content/themes/ccrc-theme/templates/searchform.php
+++ wp-content/themes/ccrc-theme/templates/searchform.php
@@ -3,7 +3,14 @@
   <div class="input-group">
     <input type="search" value="<?php echo get_search_query(); ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search the site', 'roots'); ?>">
     <span class="input-group-btn">
-      <button type="submit" class="search-submit btn btn-default"><?php _e('', 'roots'); ?><img src="<?php bloginfo('template_directory'); ?>/assets/img/search-icon.png"></button>
+      <button type="submit" class="search-submit btn btn-default"><?php _e('', 'roots'); ?>
+
+
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid" width="55" height="53" viewBox="0 0 55 53">
+  <image xlink:href="data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAADcAAAA1CAMAAADSzYN/AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAANlBMVEUAAAAAZoUAZoUAZoUAZoUAZoUAZoUAZoUAZoUAZoUAZoUAZoUAZoUAZoUAZoUAZoUAZoUAAAAYB5iaAAAAEHRSTlMAQICvv9+PYCCfz3AQUO8wiHWX3gAAAAFiS0dEAIgFHUgAAAAJcEhZcwAACxIAAAsSAdLdfvwAAAF7SURBVEjH1ZbLgoUgCEBFLfPF9f+/9jYzaqZgj92wBA8gECZEJyCVXlLSejWbuCtgUyvOhztUXFIvKC/JTSdKMF4Ew8SInYU0iRcXbmJOa7wFNpjzn19ViE1tHY196gENjTrI444k54rZ9w6rhapqcYufwRRUti3jFQOy2C4FlGw4usEhp4qDBWdXP4pmOn3M/tjm5n4oWr1ymNhywE6dP4LJx5YHHs7aXGceE56qKMyrwh4Brj1DSpri4ksOXnL+JTe730Y1PlDOzmJI13k8J8vHkqVT9Ng2GSE5UeZqYCS9Y0KaV7SEGyqX00dmsstaG8ww3ZLFSgywmoAGuXB7X4vNDZtpLXuQfF58tZ6fLajrk2nUsdJR1oRM97BRYOt4UVLKlXgNCTDQj+Y1KCx7GuegYR5cFewcDFTIJZ5zIcd/s11MZ4ZLMN9NXEtpUfljRq7Bn4QBoJ+cWyAl/xOEl6B6xB0gPuMq6B5yBZRPub/NYR9j+3+JlHs1v4wtOdc+2E85AAAAAElFTkSuQmCC" src="<?php bloginfo('template_directory'); ?>/assets/img/search-icon.png" width="55" height="53"/>
+</svg>
+
+      <!-- <img src="<?php bloginfo('template_directory'); ?>/assets/img/search-icon.png"></button> -->
     </span>
   </div>
 </form>
