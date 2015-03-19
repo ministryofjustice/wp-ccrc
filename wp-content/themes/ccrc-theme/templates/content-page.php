40f525dee6a51d288ba07c6ed65afb83bd9f39dd
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-03-19 15:52:53 +0000

    Added child/sibling left nav to pages that have child/sibling pages.

--- wp-content/themes/ccrc-theme/templates/content-page.php
+++ wp-content/themes/ccrc-theme/templates/content-page.php
@@ -9,6 +9,16 @@ the_post_thumbnail('large');
   echo '<div class="img-caption">' . get_post(get_post_thumbnail_id())->post_excerpt . '</div>';
   } ?>
 
+<?php
+  if($post->post_parent)
+  $children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
+  else
+  $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
+  if ($children) { ?>
+  <ul>
+  <?php echo $children; ?>
+  </ul>
+  <?php } ?>
 
 
 
