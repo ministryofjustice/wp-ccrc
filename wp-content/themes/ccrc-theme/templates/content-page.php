427a0b9f7b2b833cdeb9aea0be72a59efaccfbc2
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-03-19 16:19:49 +0000

    Left hand column ing and nav links small screen css amends

--- wp-content/themes/ccrc-theme/templates/content-page.php
+++ wp-content/themes/ccrc-theme/templates/content-page.php
@@ -1,6 +1,7 @@
 <div class="row">
 
 	<div class="col-sm-4">
+	<div class="feat-img">
 
 		<?php
 $get_description = get_post(get_post_thumbnail_id())->post_excerpt;
@@ -8,6 +9,7 @@ the_post_thumbnail('large');
   if(!empty($get_description)){//If description is not empty show the div
   echo '<div class="img-caption">' . get_post(get_post_thumbnail_id())->post_excerpt . '</div>';
   } ?>
+  </div>
 
 <?php
   if($post->post_parent)
