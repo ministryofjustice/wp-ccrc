c4a6bfbc2dfd723edcc41aa9d1655183e405e3f9
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2019-04-05 13:56:14 +0100

    Security audit + standard build process implemented

--- web/app/themes/ccrc/assets/js/_main.js
+++ web/app/themes/ccrc/assets/js/_main.js
@@ -14,78 +14,120 @@
  * remove or comment out: add_theme_support('jquery-cdn');
  * ======================================================================== */
 
-(function($) {
+import 'bootstrap';
+
+(function ($) {
 
 // Use this variable to set up the common and page specific functions. If you
 // rename this variable, you will also need to rename the namespace below.
-var Roots = {
-  // All pages
-  common: {
-    init: function() {
-      // JavaScript to be fired on all pages
-    }
-  },
-  // Home page
-  home: {
-    init: function() {
-      // JavaScript to be fired on the home page
-    }
-  },
-  // About us page, note the change from about-us to about_us.
-  about_us: {
-    init: function() {
-      // JavaScript to be fired on the about us page
-    }
-  }
-};
+    var Roots = {
+        // All pages
+        common: {
+            init: function () {
+                // JavaScript to be fired on all pages
+            }
+        },
+        // Home page
+        home: {
+            init: function () {
+                // JavaScript to be fired on the home page
+            }
+        },
+        // Case Library
+        page_id_12: {
+            init: function () {
+                $.get(ajax_url + '?action=get_offences_terms', ccrc_run.offences);
+            }
+        },
+        // About us page, note the change from about-us to about_us.
+        about_us: {
+            init: function () {
+                // JavaScript to be fired on the about us page
+            }
+        }
+    };
 
 // The routing fires all common scripts, followed by the page specific scripts.
 // Add additional events for more control over timing e.g. a finalize event
-var UTIL = {
-  fire: function(func, funcname, args) {
-    var namespace = Roots;
-    funcname = (funcname === undefined) ? 'init' : funcname;
-    if (func !== '' && namespace[func] && typeof namespace[func][funcname] === 'function') {
-      namespace[func][funcname](args);
-    }
-  },
-  loadEvents: function() {
-    UTIL.fire('common');
+    var UTIL = {
+        fire: function (func, funcname, args) {
+            var namespace = Roots;
+            funcname = (funcname === undefined) ? 'init' : funcname;
+            if (func !== '' && namespace[func] && typeof namespace[func][funcname] === 'function') {
+                namespace[func][funcname](args);
+            }
+        },
+        loadEvents: function () {
+            UTIL.fire('common');
 
-    $.each(document.body.className.replace(/-/g, '_').split(/\s+/),function(i,classnm) {
-      UTIL.fire(classnm);
-    });
-  }
-};
+            $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function (i, classnm) {
+                UTIL.fire(classnm);
+            });
+        }
+    };
 
-$(document).ready(UTIL.loadEvents);
+    $(document).ready(UTIL.loadEvents);
+
+    //Add class to last menu item for old IE
+    $('.navbar-default .navbar-nav li:last-child a, .news ul li:last-child').addClass('last-child');
 
 })(jQuery); // Fully reference jQuery after this point.
 
-//Add class to last menu item for old IE
+var ccrc_run = {
+    offences: function (data, text_status) {
+        if (text_status === "success") {
+            var array = [], item;
+            for (item in data) {
+                array.push(ccrc_run.entities(data[item]));
+            }
 
-$(document).ready(function (){
-  $('.navbar-default .navbar-nav li:last-child a, .news ul li:last-child').addClass('last-child');
+            $('.typeahead').autocomplete({
+                source: array,
+                autoFocus: true,
+                create: ccrc_run.offenceCountInit,
+                open: ccrc_run.offenceCount,
+                close: ccrc_run.offenceCountDestroy
+            });
+        }
+    },
+    offenceCountInit: function(event, ui) {
+        var len = $('.ui-autocomplete > li').length, ele, digit, text;
+        digit = $('<span />').attr('class', 'digit').text(len);
+        text = $('<span />').attr('class', 'text').text(' results)');
+        ele = $('<span />')
+            .attr('id', 'offence_counter')
+            .html('&nbsp;&nbsp;&nbsp;(')
+            .append(digit)
+            .append(text);
 
-});
+        $('#prefetch label').append(ele);
+        if (len === 0) {
+            $('#offence_counter').hide();
+        }
+        console.log('created #offence_counter');
+    },
+    offenceCount: function (event, ui) {
+        var len = $('.ui-autocomplete > li').length;
 
+        if ($('#offence_counter').length === 0) {
+            ccrc_run.offenceCountInit(null, null);
+        }
 
-var offences = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  limit: 10,
-  prefetch: {
-    url: ajax_url + '?action=get_offences_terms',
-    filter: function(list) {
-      return $.map(list, function(country) { return { name: country }; });
+        if (len > 0) {
+            $('#offence_counter').show();
+            $('#offence_counter .digit').text(len);
+        }
+        else {
+            $('#offence_counter').hide();
+        }
+
+    },
+    offenceCountDestroy: function(event, ui) {
+        $('#offence_counter').remove();
+    },
+    entities: function (string) {
+        var textarea = document.createElement('textarea');
+        textarea.innerHTML = string;
+        return textarea.value;
     }
-  }
-});
-
-offences.initialize();
-
-$('#prefetch .typeahead').typeahead(null, {
-  name: 'offences',
-  displayKey: 'name',
-  source: offences.ttAdapter()
-});
+};
--- web/app/themes/ccrc/lib/scripts.php
+++ web/app/themes/ccrc/lib/scripts.php
@@ -14,66 +14,75 @@
  * - An ID has been defined in config.php
  * - You're not logged in as an administrator
  */
-function roots_scripts() {
-  /**
-   * The build task in Grunt renames production assets with a hash
-   * Read the asset names from assets-manifest.json
-   */
-  /*if (WP_ENV === 'development') {
+function roots_scripts()
+{
+    /**
+     * The build task in Grunt renames production assets with a hash
+     * Read the asset names from assets-manifest.json
+     */
+    /*if (WP_ENV === 'development') {
+      $assets = array(
+        'css'       => '/assets/css/main.css',
+        'js'        => '/assets/js/scripts.js',
+        'modernizr' => '/assets/vendor/modernizr/modernizr.js',
+        'jquery'    => '//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js'
+      );
+    } else {*/
+    $get_assets = file_get_contents(get_template_directory() . '/dist/mix-manifest.json');
+    $assets = json_decode($get_assets, true);
     $assets = array(
-      'css'       => '/assets/css/main.css',
-      'js'        => '/assets/js/scripts.js',
-      'modernizr' => '/assets/vendor/modernizr/modernizr.js',
-      'jquery'    => '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js'
+        'css' => '/dist' . $assets['/css/main.min.css'],
+        'js' => '/dist' . $assets['/js/main.min.js'],
+        //'modernizr' => '/assets/vendor/modernizr/modernizr.js',
+        'jquery' => '//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'
     );
-  } else {*/
-    $get_assets = file_get_contents(get_template_directory() . '/assets/manifest.json');
-    $assets     = json_decode($get_assets, true);
-    $assets     = array(
-      'css'       => '/assets/css/main.min.css?' . $assets['assets/css/main.min.css']['hash'],
-      'js'        => '/assets/js/scripts.min.js?' . $assets['assets/js/scripts.min.js']['hash'],
-      'modernizr' => '/assets/vendor/modernizr/modernizr.js',
-      'jquery'    => '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'
-    );
-  //}
+    //}
 
-  wp_enqueue_style('roots_css', get_template_directory_uri() . $assets['css'], false, null);
+    wp_enqueue_style('roots_css', get_template_directory_uri() . $assets['css'], false, null);
 
-  /**
-   * jQuery is loaded using the same method from HTML5 Boilerplate:
-   * Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
-   * It's kept in the header instead of footer to avoid conflicts with plugins.
-   */
-  if (!is_admin() && current_theme_supports('jquery-cdn')) {
-    wp_deregister_script('jquery');
-    wp_register_script('jquery', $assets['jquery'], array(), null, false);
-    add_filter('script_loader_src', 'roots_jquery_local_fallback', 10, 2);
-  }
+    /**
+     * jQuery is loaded using the same method from HTML5 Boilerplate:
+     * Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
+     * It's kept in the header instead of footer to avoid conflicts with plugins.
+     */
+    if (!is_admin() && current_theme_supports('jquery-cdn')) {
+        wp_deregister_script('jquery');
+        wp_register_script('jquery', $assets['jquery'], array(), null, false);
+        add_filter('script_loader_src', 'roots_jquery_local_fallback', 10, 2);
 
-  if (is_single() && comments_open() && get_option('thread_comments')) {
-    wp_enqueue_script('comment-reply');
-  }
+        wp_deregister_script( 'jquery-migrate' );
+        wp_register_script( 'jquery-migrate', '//code.jquery.com/jquery-migrate-3.0.1.min.js', array( 'jquery' ), '3.0.1', false );
+        wp_enqueue_script( 'jquery-migrate' );
+    }
 
-  wp_enqueue_script('modernizr', get_template_directory_uri() . $assets['modernizr'], array(), null, false);
-  wp_enqueue_script('jquery');
-  wp_enqueue_script('roots_js', get_template_directory_uri() . $assets['js'], array(), null, true);
-  wp_localize_script('roots_js', 'ajax_url', admin_url('admin-ajax.php'));
+    if (is_single() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
+    }
+
+    //wp_enqueue_script('modernizr', get_template_directory_uri() . $assets['modernizr'], array(), null, false);
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('jquery-ui-autocomplete');
+    wp_enqueue_script('roots_js', get_template_directory_uri() . $assets['js'], array('jquery'), null, true);
+    wp_localize_script('roots_js', 'ajax_url', admin_url('admin-ajax.php'));
 }
+
 add_action('wp_enqueue_scripts', 'roots_scripts', 100);
 
 // http://wordpress.stackexchange.com/a/12450
-function roots_jquery_local_fallback($src, $handle = null) {
-  static $add_jquery_fallback = false;
+function roots_jquery_local_fallback($src, $handle = null)
+{
+    static $add_jquery_fallback = false;
 
-  if ($add_jquery_fallback) {
-    echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/vendor/jquery/dist/jquery.min.js?1.11.1"><\/script>\')</script>' . "\n";
-    $add_jquery_fallback = false;
-  }
+    if ($add_jquery_fallback) {
+        echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/vendor/jquery/dist/jquery.min.js?1.11.1"><\/script>\')</script>' . "\n";
+        $add_jquery_fallback = false;
+    }
 
-  if ($handle === 'jquery') {
-    $add_jquery_fallback = true;
-  }
+    if ($handle === 'jquery') {
+        $add_jquery_fallback = true;
+    }
 
-  return $src;
+    return $src;
 }
+
 add_action('wp_head', 'roots_jquery_local_fallback');
