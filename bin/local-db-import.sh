48d8132a85fe0405635933feb3394fa63d603e39
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2019-04-26 14:51:42 +0100

    Remove --yes from DB reset command

--- bin/local-db-import.sh
+++ bin/local-db-import.sh
@@ -2,11 +2,12 @@
 set -e
 
 # reset
-wp --allow-root db reset --yes
+wp --allow-root db reset
 
-# only run one .sql file
 for i in *.sql; do
     wp --allow-root db import "$i"
+
+    # only run the first .sql file
     break
 done
 
@@ -15,7 +16,7 @@ read -p "Enter the URL of the site most used in the backup. This will be like ht
 echo "The URL entered was: $IMPORTED_SERVER_NAME"
 echo "To be replaced with: $WP_HOME"
 
-read -p "OK to continue? (y/n) " CAN_CONTINUE
+read -p "OK to continue? [y/n] " CAN_CONTINUE
 
 if [[ "$CAN_CONTINUE" =~ "y" ]]; then
 	wp --allow-root search-replace "$IMPORTED_SERVER_NAME" "http://$WP_HOME/" --recurse-objects --skip-columns=guid --skip-tables=wp_users
@@ -26,7 +27,7 @@ if [[ "$CAN_CONTINUE" =~ "y" ]]; then
 	echo "URL to find: $NO_PROTOCOL_IMPORTED_NAME"
     echo "To be replaced with: $VIRTUAL_HOST"
 
-    read -p "OK to continue? (y/n) " CAN_CONTINUE_2
+    read -p "OK to continue? [y/n] " CAN_CONTINUE_2
 
     if [[ "$CAN_CONTINUE_2" =~ "y" ]]; then
 	    wp --allow-root search-replace "$NO_PROTOCOL_IMPORTED_NAME" "$VIRTUAL_HOST" --recurse-objects --skip-columns=guid --skip-tables=wp_users
